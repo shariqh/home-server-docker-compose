f771b4b316617626fc8afffbc4b3e718f057903a
Shariq Hirani <9244782+shariqh@users.noreply.github.com>
2021-08-10 10:21:17 -0500

    added prune step

--- containerupdater.sh
+++ containerupdater.sh
@@ -2,6 +2,8 @@ for file in *-compose.yml
 do
     # pull all updated images
     docker-compose -f "$file" pull
-    # remove all old images
+    # restart containers with latest versions
     docker-compose -f "$file" up -d
+    # remove images
+    docker image prune
 done
