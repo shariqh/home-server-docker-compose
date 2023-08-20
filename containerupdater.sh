165db74715362bc928e99b355200486ea475712e
Shariq Hirani <shariqhirani2010@gmail.com>
2023-08-20 13:51:58 -0700

    added readarr, cleaned up nginx stuff

--- containerupdater.sh
+++ containerupdater.sh
@@ -1,9 +1,9 @@
 for file in *-compose.yml
 do
     # pull all updated images
-    docker-compose -f "$file" pull
+    docker compose -f "$file" pull
     # restart containers with latest versions
-    docker-compose -f "$file" up -d
+    docker compose -f "$file" up -d
     # remove images
     docker image prune -f -a
 done
